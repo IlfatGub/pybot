ea9317dfc3bf5aac6bc1882a4fc16ba65864c5ae
ilfat <ilfat.gubaidullin@gmail.com>
2023-08-27 19:00:01 +0500

    -

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-print("asdas")
 
-import telebot;
+import telebot
+from telebot import types
 
 bot = telebot.TeleBot("asdasd")
 bot.polling(none_stop=True, interval=0)
