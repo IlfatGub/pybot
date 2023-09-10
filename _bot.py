27d80126d397e05b33fe9bbd9ffc7be1e51535f0
ilfat <ilfat.gubaidullin@gmail.com>
2023-09-10 21:18:42 +0500

    -

--- _bot.py
+++ _bot.py
@@ -4,14 +4,22 @@
 import asyncio, os, logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
-from pyconfig import config
+# from pyconfig import config
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
-bot = Bot(token=config.bot_token.get_secret_value())
+bot = Bot(token=os.environ['TOKEN'])
 # Диспетчер
 dp = Dispatcher()
+
+# Если не указать фильтр F.text, 
+# то хэндлер сработает даже на картинку с подписью /test,
+# но пока нам это не важно и рассматриваем только текстовые сообщения
+@dp.message(Command("test"))
+async def any_message(message: types.Message):
+    await message.answer("Hello, <b>world</b>!", parse_mode="HTML")
+    await message.answer("Hello, *world*\!", parse_mode="MarkdownV2")
 
 # Хэндлер на команду /start
 @dp.message(Command("start"))
