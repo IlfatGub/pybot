c2b623fde38c3e0de1e152617e5f8a54fa117f1e
ilfat <ilfat.gubaidullin@gmail.com>
2023-09-10 21:43:31 +0500

    -

--- _bot.py
+++ _bot.py
@@ -34,27 +34,31 @@ async def cmd_name(message: types.Message, command: CommandObject):
         await message.answer(f"Привет, {html.bold(html.quote(command.args))}", parse_mode="HTML")
     else:
         await message.answer("Пожалуйста, укажи своё имя после команды /name!")
-
-@dp.message(content_types=types.ContentType.TEXT)
-async def extract_data(message: types.Message):
-    data = {
-        "url": "<N/A>",
-        "email": "<N/A>",
-        "code": "<N/A>"
-    }
-    entities = message.entities or []
-    for item in entities:
-        if item.type in data.keys():
-            # Неправильно
-            # data[item.type] = message.text[item.offset : item.offset+item.length]
-            # Правильно
-            data[item.type] = item.extract_from(message.text)
-    await message.reply(
-        "Вот что я нашёл:\n"
-        f"URL: {html.quote(data['url'])}\n"
-        f"E-mail: {html.quote(data['email'])}\n"
-        f"Пароль: {html.quote(data['code'])}"
-    )
+        
+@dp.message_handler(content_types=types.ContentTypes.PHOTO)
+async def handle_photo(message: types.Message):
+    for photo in message.photo:
+        await message.reply(f"Вы отправили фотографию с размером {photo.width}x{photo.height} пикселей.")
+# @dp.message(content_types=types.ContentType.TEXT)
+# async def extract_data(message: types.Message):
+#     data = {
+#         "url": "<N/A>",
+#         "email": "<N/A>",
+#         "code": "<N/A>"
+#     }
+#     entities = message.entities or []
+#     for item in entities:
+#         if item.type in data.keys():
+#             # Неправильно
+#             # data[item.type] = message.text[item.offset : item.offset+item.length]
+#             # Правильно
+#             data[item.type] = item.extract_from(message.text)
+#     await message.reply(
+#         "Вот что я нашёл:\n"
+#         f"URL: {html.quote(data['url'])}\n"
+#         f"E-mail: {html.quote(data['email'])}\n"
+#         f"Пароль: {html.quote(data['code'])}"
+#     )
 
 # Хэндлер на команду /start
 @dp.message(Command("start"))
