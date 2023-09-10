90e216cb4b317a5617520c25a7ccb52874b8c410
ilfat <ilfat.gubaidullin@gmail.com>
2023-09-10 18:26:12 +0500

    -

--- _bot.py
+++ _bot.py
@@ -2,80 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import os
-from telegram import InlineQueryResultArticle, InputTextMessageContent
-from telegram.ext import Updater, CommandHandler
-from telegram.ext import MessageHandler, filters, InlineQueryHandler
+from aiogram import Bot, Dispatcher, executor
+import handlers
+
 print(os.environ['TOKEN'])
 
-TOKEN = os.environ['TOKEN']
-
-updater = Updater(token=TOKEN)
-dispatcher = updater.dispatcher
-
-# функция обработки команды '/start'
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, 
-                             text="I'm a bot, please talk to me!")
-
-# функция обработки текстовых сообщений
-def echo(update, context):
-    text = 'ECHO: ' + update.message.text 
-    context.bot.send_message(chat_id=update.effective_chat.id, 
-                             text=text)    
-
-# функция обработки команды '/caps'
-def caps(update, context):
-    if context.args:
-        text_caps = ' '.join(context.args).upper()
-        context.bot.send_message(chat_id=update.effective_chat.id, 
-                                 text=text_caps)
-    else:
-        context.bot.send_message(chat_id=update.effective_chat.id, 
-                                 text='No command argument')
-        context.bot.send_message(chat_id=update.effective_chat.id, 
-                                 text='send: /caps argument')
-
-# функция обработки встроенного запроса
-def inline_caps(update, context):
-    query = update.inline_query.query
-    if not query:
-        return
-    results = list()
-    results.append(
-        InlineQueryResultArticle(
-            id=query.upper(),
-            title='Convert to UPPER TEXT',
-            input_message_content=InputTextMessageContent(query.upper())
-        )
-    )
-    context.bot.answer_inline_query(update.inline_query.id, results)
-
-# функция обработки не распознных команд
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, 
-                             text="Sorry, I didn't understand that command.")
-
-# обработчик команды '/start'
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)    
-
-# обработчик текстовых сообщений
-echo_handler = MessageHandler(filters.text & (~filters.command), echo)
-dispatcher.add_handler(echo_handler)
-
-# обработчик команды '/caps'
-caps_handler = CommandHandler('caps', caps)
-dispatcher.add_handler(caps_handler)
-
-# обработчик встроенных запросов 
-inline_caps_handler = InlineQueryHandler(inline_caps)
-dispatcher.add_handler(inline_caps_handler)
-
-# обработчик не распознанных команд
-unknown_handler = MessageHandler(filters.command, unknown)
-dispatcher.add_handler(unknown_handler)
-
-# запуск прослушивания сообщений
-updater.start_polling()
-# обработчик нажатия Ctrl+C
-updater.idle()
+API_TOKEN =  os.environ['TOKEN']
+ 
+# создаем бота и диспетчер
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+ 
+# регистрируем функции
+dp.register_message_handler(handlers.start, commands=["start"])
+dp.register_message_handler(handlers.echo)
+ 
+# запускаем программу
+if __name__ == '__main__':
+# указание skip_updates=True
+# пропустит команды,
+# которые отправили
+# до старта бота
+# executor.start_polling(dp, skip_updates=True)
