0af483d390ea52eb68be22040edb259a871eb3dd
ilfat <ilfat.gubaidullin@gmail.com>
2023-09-10 18:28:10 +0500

    -

--- _bot.py
+++ _bot.py
@@ -2,25 +2,31 @@
 # -*- coding: utf-8 -*-
 
 import os
-from aiogram import Bot, Dispatcher, executor
 import handlers
+import time
+import logging
+from aiogram import Bot, Dispatcher, executor, types
 
-print(os.environ['TOKEN'])
 
-API_TOKEN =  os.environ['TOKEN']
- 
-# создаем бота и диспетчер
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
- 
-# регистрируем функции
-dp.register_message_handler(handlers.start, commands=["start"])
-dp.register_message_handler(handlers.echo)
- 
-# запускаем программу
-if __name__ == '__main__':
-# указание skip_updates=True
-# пропустит команды,
-# которые отправили
-# до старта бота
-executor.start_polling(dp, skip_updates=True)
+TOKEN = os.environ['TOKEN']
+MSG = "Программировал ли ты сегодня, {}?"
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot=bot)
+
+@dp.message_handler(commands=["start"])
+async def start_handler(message: types.Message):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    user_full_name = message.from_user.full_name
+    logging.info(f'{user_id} {user_full_name} {time.asctime()}')
+    await message.reply(f"Привет, {user_full_name}!")
+    
+    for i in range(7):
+        await asyncio.sleep(60*60*24)
+        await bot.send_message(user_id, MSG.format(user_name))
+
+if __name__ == "__main__": 
+    executor.start_polling(dp)
